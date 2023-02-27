780ec32f613cd81bd07f8b245840e06806394e31
Dorps <Dorps@users.noreply.github.com>
2023-02-27 06:28:42 -0500

    finished multi enemy fight

--- Ruined/Assets/Scenes/BattleSystem.cs
+++ Ruined/Assets/Scenes/BattleSystem.cs
@@ -11,19 +11,25 @@ public class BattleSystem : MonoBehaviour
 
 	public GameObject playerPrefab;
 	public GameObject enemyPrefab;
+	public GameObject enemy2Prefab;
 
 	public Transform playerStart;
 	public Transform enemyStart;
+	public Transform enemy2Start;
 
 	Unit playerUnit;
 	Unit enemyUnit;
+	Unit enemy2Unit;
 
 	public TextMeshProUGUI dialogueText;
 
 	public BattleHUD playerHUD;
 	public BattleHUD enemyHUD;
+	public BattleHUD enemy2HUD;
 
 	public BattleState state;
+
+	public GameObject rewardScreen;
 
 	// Start is called before the first frame update
 	void Start()
@@ -32,6 +38,22 @@ public class BattleSystem : MonoBehaviour
 		StartCoroutine(SetupBattle());
 	}
 
+	IEnumerator nextEnemyTurn(string nextEnemy)
+    {
+		if (nextEnemy == "enemy")
+        {
+			return EnemyTurn();
+        }
+		else if (nextEnemy == "enemy2") {
+			return Enemy2Turn();
+        }
+        else
+        {
+			Debug.Log("hello");
+			return SetupBattle();
+        }
+    }
+	//implement last enumerator that just starts player turn()
 	IEnumerator SetupBattle()
 	{
 		GameObject playerGO = Instantiate(playerPrefab, playerStart);
@@ -40,10 +62,14 @@ public class BattleSystem : MonoBehaviour
 		GameObject enemyGO = Instantiate(enemyPrefab, enemyStart);
 		enemyUnit = enemyGO.GetComponent<Unit>();
 
+		GameObject enemy2GO = Instantiate(enemy2Prefab, enemy2Start);
+		enemy2Unit = enemy2GO.GetComponent<Unit>();
+
 		dialogueText.text = "A wild " + enemyUnit.unitName + " approaches...";
 
 		playerHUD.SetHUD(playerUnit);
 		enemyHUD.SetHUD(enemyUnit);
+		enemy2HUD.SetHUD(enemy2Unit);
 
 		yield return new WaitForSeconds(2f);
 
@@ -53,14 +79,17 @@ public class BattleSystem : MonoBehaviour
 
 	IEnumerator PlayerAttack()
 	{
+		//check if enemy is in pattern and can just make another button that hard codes attack one
 		bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+		bool isDead2 = enemy2Unit.TakeDamage(playerUnit.damage);
 
 		enemyHUD.SetHP(enemyUnit.currentHP);
+		enemy2HUD.SetHP(enemy2Unit.currentHP);
 		dialogueText.text = "The attack is successful!";
 
 		yield return new WaitForSeconds(2f);
 
-		if (isDead)
+		if (isDead || isDead2)
 		{
 			state = BattleState.WON;
 			EndBattle();
@@ -68,19 +97,19 @@ public class BattleSystem : MonoBehaviour
 		else
 		{
 			state = BattleState.ENEMYTURN;
-			StartCoroutine(EnemyTurn());
+			StartCoroutine(nextEnemyTurn("enemy"));
 		}
 	}
 
 	IEnumerator EnemyTurn()
 	{
-		if (enemyUnit.isStunned == true)
+		if (enemyUnit.isStunned == true) 
         {
 			dialogueText.text = enemyUnit.unitName + " is stunned!";
 			yield return new WaitForSeconds(1f);
 			enemyUnit.isStunned = false;
-			state = BattleState.PLAYERTURN; //make this next turn function
-			PlayerTurn();
+			state = BattleState.ENEMYTURN; //make this next turn function
+			StartCoroutine(nextEnemyTurn("enemy2")); ;  //here would call next enemy turn
 		}
         else
         {
@@ -101,11 +130,48 @@ public class BattleSystem : MonoBehaviour
 			}
 			else
 			{
+				state = BattleState.ENEMYTURN;
+				StartCoroutine(nextEnemyTurn("enemy2")); ;
+			}
+		}
+		
+
+	}
+
+	IEnumerator Enemy2Turn()
+	{
+		if (enemyUnit.isStunned == true)
+		{
+			dialogueText.text = enemy2Unit.unitName + " is stunned!";
+			yield return new WaitForSeconds(1f);
+			enemyUnit.isStunned = false;
+			state = BattleState.PLAYERTURN; //make this next turn function
+			PlayerTurn();
+		}
+		else
+		{
+			dialogueText.text = enemy2Unit.unitName + " attacks!";
+
+			yield return new WaitForSeconds(1f);
+
+			bool isDead = playerUnit.TakeDamage(enemy2Unit.damage);
+
+			playerHUD.SetHP(playerUnit.currentHP);
+
+			yield return new WaitForSeconds(1f);
+
+			if (isDead)
+			{
+				state = BattleState.LOST;
+				EndBattle();
+			}
+			else
+			{
 				state = BattleState.PLAYERTURN;
 				PlayerTurn();
 			}
 		}
-		
+
 
 	}
 
@@ -114,6 +180,8 @@ public class BattleSystem : MonoBehaviour
 		if (state == BattleState.WON)
 		{
 			dialogueText.text = "You won the battle!";
+			rewardScreen.SetActive(true);
+
 		}
 		else if (state == BattleState.LOST)
 		{
@@ -136,11 +204,12 @@ public class BattleSystem : MonoBehaviour
 		yield return new WaitForSeconds(2f);
 
 		state = BattleState.ENEMYTURN;
-		StartCoroutine(EnemyTurn());
+		StartCoroutine(nextEnemyTurn("enemy"));
 	}
 
 	IEnumerator PlayerStun()
 	{
+		//check if enemy is in squares, also //stuns only one enemy
 		enemyUnit.isStunned = true;
 
 		dialogueText.text = "You stunned " + enemyUnit.unitName;
@@ -149,7 +218,7 @@ public class BattleSystem : MonoBehaviour
 		
 
 		state = BattleState.ENEMYTURN;
-		StartCoroutine(EnemyTurn());
+		StartCoroutine(nextEnemyTurn("enemy"));
 	}
 
 	public void OnAttackButton()
